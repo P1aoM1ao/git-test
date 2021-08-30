5506310019a174584e47cc3c133a3be48a2ea37d
zhongcheng <zhongcheng@qdairlines.com>
2021-08-30 13:57:57 +0800

    FirstTest.java

---
+++ git-test-project/src/com/test/git/FirstTest.java
@@ -0,0 +1,10 @@
+package com.test.git;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
